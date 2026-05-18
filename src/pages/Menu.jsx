67ab6b84627c5bf4447542de759d8eb3e9243235
manager <manager@spdevgroup.com>
2026-05-18 07:15:07 +0000

    DataSourceListener3 Implementation and UI

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -44,7 +44,7 @@ export default function Menu() {
           Data Master Listener
         </Button>
         <Button variant="outline" className="w-full" onClick={() => navigate("/datasourcelistener2")}>
-          Data Source 5 min Refresh 3
+          Data Source 5 minute Refresh
         </Button>
         <Button variant="outline" className="w-full" onClick={() => navigate("/datasourcelistener3")}>
           Data Source Listener 3
